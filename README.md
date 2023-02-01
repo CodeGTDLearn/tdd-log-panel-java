c8e320d27518d4d19483fe9a9e5c4e1db6300d32
PauloPortofolio <pvba02@gmail.com>
2023-01-31 19:12:15 -0500

    05) Project Concluded
    -> CONCLUDED:
    Console Log border done
    README.md improvements - add screenshots examples
    adding Items Bullet Point
    make Horizontal lines configurable
    make verticallines configurable
    Refactoring to imdecrease the amount of Enums
    
    -> WORKING:
    
    -> NEXT STEPS:
    
    -> FUTURE IMPLEMENTATIONS:

--- README.md
+++ README.md
@@ -3,21 +3,36 @@
 
 ### Index
 
-* [StackOverFlow Question](#stackoverflow-question)
-* [ASCII Source](#ascii-source)
+* [Disclaimer](#Disclaimer)
+* [Objective](#objective)
+* [StackOverFlow Requests](#stackoverflow-requests)
+* [ASCII Sources](#ascii-sources)
 * [Console Log Examples](#console-log-examples)
 
+### Disclaimer
+1. The strucuture of this app was done without any refactoring
+2. Hence, it is possible to find some 'boiler-plate' integrating this solution.
+ 
+### Objective
+1. This little-app creates Log messages using bolders in console;
+2. The objective of this solution is to solve a StackOVerFlow requests.
+3. The main idea were to solve the problem using the most configurable solution found.
 
-### StackOverFlow Question
-1. https://stackoverflow.com/questions/32790698/console-output-to-frame-text-area
-2. https://stackoverflow.com/questions/20244554/creating-a-border-with-system-out-println
+### StackOverFlow Requests
+1. [Console-output-to-frame-text-area](https://stackoverflow.com/questions/32790698/console-output-to-frame-text-area)
+2. [Creating-a-border-with-system-out-println](https://stackoverflow.com/questions/20244554/creating-a-border-with-system-out-println)
 
-### ASCII Source
+### ASCII Sources
 1. Box Draw Special Characteres:
    * https://en.wikipedia.org/wiki/Box-drawing_character
 2. Unicode symbols:
    * https://en.wikipedia.org/wiki/Unicode_symbols
    * https://en.wikipedia.org/wiki/Bullet_(typography) -- bullet
+3. Ordinal indicator:
+  * https://en.wikipedia.org/wiki/Ordinal_indicator
 
 ### Console Log Examples
-![img.png](img.png)
+![img.png](img.png) 
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
+![img_3.png](img_3.png) 
--- src/main/java/com/com/tddconsolelog/panel/ConsolePanel.java
+++ src/main/java/com/com/tddconsolelog/panel/ConsolePanel.java
@@ -7,24 +7,32 @@ public class ConsolePanel {
 
   public static void main(String[] args) {
 
-    panelCreator(
+    panel(
          21,
-         5,
+         2,
          3,
          3,
+         Border.BOLD,
+         Border.DOUBLE,
+         Border.DOUBLE,
+         Border.THIN,
          true,
          true,
-         "myTitcc cccc   cccc ccle dddddd", "myBvvy2", "myBvvy2"
+         "My Title", "My First Topic text", "My Second Topic text"
     );
   }
 
   public static void simplePanel(String... texts) {
 
-    panelCreator(
+    panel(
          21,
          5,
          1,
          1,
+         Border.DOUBLE,
+         Border.DOUBLE,
+         Border.THIN,
+         Border.THIN,
          true,
          true,
          texts
@@ -33,22 +41,30 @@ public class ConsolePanel {
 
   public static void simplePanelScalable(int scale, String... texts) {
 
-    panelCreator(
+    panel(
          scale,
          5,
          1,
          1,
+         Border.DOUBLE,
+         Border.DOUBLE,
+         Border.THIN,
+         Border.THIN,
          true,
          true,
          texts
     );
   }
 
-  public static void panelCreator(
+  public static void panel(
        int scale,
        int margin,
        int upSpace,
        int downSpace,
+       Border cornersFormat,
+       Border centerMarksFormat,
+       Border horizontalLinesFormat,
+       Border verticalLinesFormat,
        boolean uppercaseTitle,
        boolean centralizeTitle,
        String... titleAndOthers) {
@@ -74,36 +90,45 @@ public class ConsolePanel {
     if (fullSize % 2 == 0) ++ fullSize;
     else -- fullSize;
 
-    var internalTextArea = String.valueOf(fullSize);
-
     var whitespaceMargin = " ".repeat(marginLimitedBySize);
     var externalUpSpaces = "\n".repeat(upSpace);
     var externalBottomSpaces = "\n".repeat(downSpace);
 
-    var upperBorder = upperLine(scale, Border.DOUBLE, Border.BOLD, Border.THIN);
-    var divider = middleLine(scale, Border.DOUBLE, Border.DOUBLE, Border.THIN);
-    var bottomBorder = bottomLine(scale, Border.DOUBLE, Border.BOLD, Border.THIN);
+    var upperFace = upperLine(scale, cornersFormat, centerMarksFormat, horizontalLinesFormat);
+    var divider = middleLine(scale, cornersFormat, centerMarksFormat, horizontalLinesFormat);
+    var bottomFace = bottomLine(scale, cornersFormat, centerMarksFormat, horizontalLinesFormat);
+    var faceLine = faceLine(verticalLinesFormat);
 
-    var builder = new StringBuilder();
-    builder.append(externalUpSpaces)
-           .append(upperBorder)
-           .append(ThinFont.FACE_LINE.code)
-           .append("%s%%-%ss".formatted(whitespaceMargin, internalTextArea))
-           .append(ThinFont.FACE_LINE.code)
-           .append("\n")
-           .append(divider);
+    var titleTextArea = String.valueOf(fullSize);
+    var textPreparation = new StringBuilder();
+    textPreparation.append(externalUpSpaces)
+                   .append(upperFace)
+                   .append(faceLine)
+                   .append("%s%%-%ss".formatted(whitespaceMargin, titleTextArea))
+                   .append(faceLine)
+                   .append("\n")
+                   .append(divider);
 
     // "-1" Because the first element in the Array was used as title
-    for (int i = formattedTexts.length - 1; i > 0; i--)
-      //https://en.wikipedia.org/wiki/Bullet_(typography)
-      builder.append(ThinFont.FACE_LINE.code)
-             .append("%s%%-%ss".formatted(whitespaceMargin, internalTextArea))
-             .append(ThinFont.FACE_LINE.code)
-             .append("\n");
-
-    builder.append(bottomBorder)
-           .append(externalBottomSpaces);
-    System.out.printf(builder.toString(), formattedTexts);
+    // The discont-number in bodyTextArea/fullsize, subtract the size of "ordinal-ASC" and ") "
+    var bodyTextArea = String.valueOf(fullSize - 4);
+    var topicEnumeration = 0;
+    var ordinalSymbolEnumerator = '\u2070';
+    for (int i = formattedTexts.length - 1; i > 0; i--) {
+      ++ topicEnumeration;
+      textPreparation.append(faceLine)
+                     .append("%s%s%s) %%-%ss".formatted(
+                          whitespaceMargin,
+                          topicEnumeration,
+                          ordinalSymbolEnumerator,
+                          bodyTextArea
+                     ))
+                     .append(faceLine)
+                     .append("\n");
+    }
+    textPreparation.append(bottomFace)
+                   .append(externalBottomSpaces);
+    System.out.printf(textPreparation.toString(), formattedTexts);
   }
 
   private static String generateLine(char baseChar, int scale, char BASE_LINE) {
@@ -119,7 +144,7 @@ public class ConsolePanel {
        int scale,
        Border corner,
        Border centerMark,
-       Border baseLine) {
+       Border line) {
 
     ArrayList<Character> borderStylingItems = new ArrayList<>();
     switch (corner) {
@@ -143,7 +168,7 @@ public class ConsolePanel {
       case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
     }
 
-    switch (baseLine) {
+    switch (line) {
       case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
       case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
       case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
@@ -237,6 +262,15 @@ public class ConsolePanel {
     return borderStylingItems.get(0) + baseline +
            borderStylingItems.get(2) + baseline +
            borderStylingItems.get(1) + "\n";
+  }
+
+  private static Character faceLine(Border corner) {
+
+    return switch (corner) {
+      case BOLD -> BoldFont.FACE_LINE.code;
+      case THIN -> ThinFont.FACE_LINE.code;
+      case DOUBLE -> DoubleFont.FACE_LINE.code;
+    };
   }
 
   private enum BoldFont {
